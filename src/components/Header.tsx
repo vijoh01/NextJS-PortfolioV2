691856a1f6c9289377e3d8df1159e86077688884
vijoh01 <vijoh01@gmail.com>
2024-11-03 13:48:37 +0100

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,11 +7,11 @@ import { PiArrowSquareOutThin } from "react-icons/pi";
 
 const Header = () => {
   return (
-    <div className="w-full bg-primary-light dark:bg-primary-dark flex flex-col items-center justify-between overflow-hidden h-[55rem]">
+    <div className="w-full bg-primary-light dark:bg-primary-dark flex flex-col items-center justify-between overflow-hidden h-[100vh]">
       <div></div>
         <div className="flex items-center md:justify-evenly md:max-w-[60rem] flex-col-reverse md:flex-row w-full">
       <div className="mt-10 md:mt-10 max-w-[17rem] sm:max-w-[27rem] sm:mr-0 md:mr-24">
-        <h1 className="w-full md:mt-0 text-3xl sm:text-5xl text-primary-dark dark:text-primary-light">Code and Design, I Make Ideas Real.</h1>
+        <h1 className="w-full mt-10 md:mt-0 sm:mt-0 text-3xl sm:text-5xl text-primary-dark dark:text-primary-light">Code and Design, I Make Ideas Real.</h1>
         <p className="mt-5 mb-5 text-primary-dark dark:text-primary-light">Discover my experience through my latest projects.</p>
         <div className="flex items-center mb-10">
         <a href={"/api/download"} className="flex justify-between items-center bg-primary-dark text-primary-light dark:bg-primary-light dark:text-primary-dark py-2 px-5 rounded-md btn-text font-bold" download>Resume<PiArrowSquareOutThin className="ml-2 fill-primary-light size-4 dark:fill-primary-dark"/></a>
